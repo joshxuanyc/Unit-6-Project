2ac63bf5d87680a1db3be5c5c0445617e01009f6
joshxuanyc <95878862+joshxuanyc@users.noreply.github.com>
2022-06-14 13:10:51 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,328 @@
+$(".button").click(function() {
+    $(".firstImage").hide();
+});
+$(".button").click(function() {
+    $(".title") .hide();
+});
+$(".button").click(function() {
+    $(".button") .hide();
+     $(".MathAnswers1") .hide();
+      $(".MathAnswers2") .hide();
+    
+          $(".math1") .hide();
+      $(".math2") .hide();
+         $(".NextQuestion1") .hide();
+ $(".NextQuestion2") .hide();
+    
+          $(".GrammerAnswers2") .hide();
+      $(".NextQuestion3") .hide();
+      $(".grammer2") .hide();
+    
+$(".SpellingAnswers2") .hide();
+       $(".spelling2") .hide();
+    
+ 	   $(".reading3") .hide();
+       $(".ReadingAnswers3") .hide();
+       $(".NextQuestion4") .hide();
+    
+      $(".ScienceAnswers3") .hide();
+      $(".science3") .hide();
+      $(".NextQuestion5") .hide();
+
+        $(".ScienceAnswers4") .hide();
+       $(".science4") .hide();
+           $(".NextQuestion6") .hide();
+    
+    	   $(".HistoryAnswer5") .hide();
+    	   $(".history5") .hide();
+	   $(".NextQuestion7") .hide();
+    
+     $(".GeographyAnswer5") .hide();
+       $(".geography5") .hide();
+           $(".NextQuestion8") .hide();
+    
+           $(".thanks") .hide();
+               $(".before") .hide();
+           $(".before1") .hide();
+               $(".before2") .hide();
+
+           $(".thanks1") .hide();
+});
+
+$(".button1").click(function() {
+    $(".button2") .hide();
+    $(".button3") .hide();
+    $(".button4") .hide();
+    $(".button5") .hide();
+    $(".button6") .hide();
+    $(".button7") .hide();
+    $(".button8") .hide();
+    $(".button9") .hide();
+    $(".MathAnswers1").show();
+      $(".math1").show();
+
+    
+});
+
+$(".q1a").click(function() {
+    $(".q1b") .hide();
+    $(".q1c") .hide();
+    $(".q1d") .hide();
+    $(".NextQuestion1").show();
+});
+$(".q1b").click(function() {
+        $(".q1b") .hide();
+
+});
+$(".q1c").click(function() {
+        $(".q1c") .hide();
+
+});
+$(".q1d").click(function() {
+        $(".q1d") .hide();
+
+});
+
+
+
+$(".NextQuestion1").click(function() {
+       $(".MathAnswers1") .hide();
+       $(".math1") .hide();
+       $(".button1") .hide();
+       $(".button2") .show();
+		$(".NextQuestion1") .hide();
+
+});
+
+
+$(".q2d").click(function() {
+       $(".q2a") .hide();
+       $(".q2b") .hide();
+       $(".q2c") .hide();
+       $(".button2") .hide();
+       $(".q2d") .hide();
+	   $(".MathAnswers2") .hide();
+	   $(".NextQuestion2") .show();
+});
+
+
+$(".q2a").click(function() {
+        $(".q1a") .hide();
+
+});
+$(".q2c").click(function() {
+        $(".q1b") .hide();
+
+});
+$(".q2c").click(function() {
+        $(".q2c") .hide();
+});
+
+
+
+$(".button2").click(function() {
+       $(".MathAnswers2") .show();
+       $(".math2") .show();
+
+});
+
+$(".NextQuestion2").click(function() {
+           $(".GrammerAnswers2") .show();
+       $(".grammer2") .show();
+});
+
+
+
+$(".q3b").click(function() {
+       $(".q3a") .hide();
+       $(".q3c") .hide();
+       $(".q3d") .hide();
+       $(".button3") .hide();
+       $(".q3b") .hide();
+	   $(".GrammerAnswers2") .hide();
+    	   $(".NextQuestion2") .hide();
+	   $(".NextQuestion4") .show();
+});
+
+$(".q3a").click(function() {
+        $(".q3a") .hide();
+
+});
+$(".q3c").click(function() {
+        $(".q3c") .hide();
+
+});
+$(".q3d").click(function() {
+        $(".q3d") .hide();
+});
+
+
+
+$(".NextQuestion3").click(function() {
+           $(".SpellingAnswers2") .show();
+       $(".spelling2") .show();
+});
+
+$(".NextQuestion4").click(function() {
+           $(".ReadingAnswers3") .show();
+       $(".reading3") .show();
+});
+
+$(".q4a").click(function() {
+       $(".q4b") .hide();
+       $(".q4c") .hide();
+       $(".q4d") .hide();
+       $(".button4") .hide();
+       $(".q4a") .hide();
+	   $(".ReadingAnswers3") .hide();
+    	   $(".NextQuestion4") .hide();
+	   $(".NextQuestion5") .show();
+});
+
+$(".q4b").click(function() {
+        $(".q4b") .hide();
+
+});
+$(".q4c").click(function() {
+        $(".q4c") .hide();
+
+});
+$(".q4d").click(function() {
+        $(".q4d") .hide();
+});
+
+
+
+
+$(".NextQuestion5").click(function() {
+           $(".ScienceAnswers3") .show();
+       $(".science3") .show();
+           $(".NextQuestion5") .hide();
+
+});
+
+$(".q5a").click(function() {
+       $(".q5d") .hide();
+       $(".q5b") .hide();
+       $(".q5c") .hide();
+       $(".button5") .hide();
+       $(".q5a") .hide();
+	   $(".ScienceAnswers3") .hide();
+    	   $(".science4") .hide();
+	   $(".NextQuestion6") .show();
+});
+
+$(".q5b").click(function() {
+        $(".q5b") .hide();
+
+});
+$(".q5c").click(function() {
+        $(".q5c") .hide();
+
+});
+$(".q5d").click(function() {
+        $(".q5d") .hide();
+});
+
+
+
+$(".NextQuestion6").click(function() {
+           $(".ScienceAnswers4") .show();
+       $(".science4") .show();
+           $(".NextQuestion6") .hide();
+});
+
+
+$(".q6d").click(function() {
+       $(".q6a") .hide();
+       $(".q6b") .hide();
+       $(".q6c") .hide();
+       $(".button6") .hide();
+       $(".q6d") .hide();
+	   $(".ScienceAnswers4") .hide();
+    	   $(".science4") .hide();
+        	   $(".NextQuestion7") .hide();
+    	   $(".NextQuestion8") .show();
+});
+
+
+
+$(".q6a").click(function() {
+        $(".q6a") .hide();
+
+});
+$(".q6b").click(function() {
+        $(".q6b") .hide();
+
+});
+$(".q3c").click(function() {
+        $(".q3c") .hide();
+});
+
+
+
+$(".NextQuestion7").click(function() {
+           $(".HistoryAnswer5") .show();
+       $(".history5") .show();
+           $(".NextQuestion7") .hide();
+});
+
+$(".q7c").click(function() {
+       $(".q7a") .hide();
+       $(".q7b") .hide();
+       $(".q7d") .hide();
+       $(".button7") .hide();
+       $(".q7c") .hide();
+	   $(".HistoryAnswer5") .hide();
+    	   $(".history5") .hide();
+	   $(".NextQuestion8") .show();
+});
+
+
+$(".q7a").click(function() {
+        $(".q7a") .hide();
+
+});
+$(".q7b").click(function() {
+        $(".q7b") .hide();
+
+});
+$(".q7d").click(function() {
+        $(".7d") .hide();
+});
+
+
+
+$(".NextQuestion8").click(function() {
+           $(".GeographyAnswer5") .show();
+       $(".geography5") .show();
+           $(".NextQuestion8") .hide();
+});
+$(".q8b").click(function() {
+       $(".q8a") .hide();
+       $(".q8c") .hide();
+       $(".q8d") .hide();
+       $(".button7") .hide();
+       $(".q8b") .hide();
+	   $(".GeographyAnswer5") .hide();
+    	   $(".geography5") .hide();
+        	   $(".thanks") .show();
+        	   $(".thanks1") .show();
+});
+
+$(".q8a").click(function() {
+        $(".q8a") .hide();
+
+});
+$(".q8c").click(function() {
+        $(".q8c") .hide();
+
+});
+$(".q8d").click(function() {
+        $(".q8d") .hide();
+});
+
+
+$(".yes-button").click(function() {
+    $(".step2"). show();
+});
